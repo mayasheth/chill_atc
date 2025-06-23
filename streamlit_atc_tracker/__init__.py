7e1f4220e3d609ac69f311c4416dba7f3354e020
Maya Sheth <shethm@F5F7G1QKNY-2.local>
2025-06-23 09:15:23 -0700

    update custom component

--- streamlit_atc_tracker/__init__.py
+++ streamlit_atc_tracker/__init__.py
@@ -1,15 +1,10 @@
-import streamlit.components.v1 as components
 import os
+import streamlit.components.v1 as components
 
-# Declare the component (set `path` to local frontend directory if developing)
 _component_func = components.declare_component(
     "atc_tracker",
-    path=os.path.join(os.path.dirname(__file__), "frontend")
+    path=os.path.join(os.path.dirname(__file__), "frontend", "build")
 )
 
 def atc_tracker(update_interval=60, stream_url=None):
-    """
-    Render ATC audio player with given stream URL and report playtime to Streamlit
-    every `update_interval` seconds.
-    """
     return _component_func(update_interval=update_interval, stream_url=stream_url, default=0)
