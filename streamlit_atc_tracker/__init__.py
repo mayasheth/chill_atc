af0acfb80e1cd001157b5835016ea2d42bbc448b
Maya Sheth <shethm@DNa207969.SUNet>
2025-06-24 19:28:39 -0700

    update custom component

--- streamlit_atc_tracker/__init__.py
+++ streamlit_atc_tracker/__init__.py
@@ -3,8 +3,8 @@ import streamlit.components.v1 as components
 
 _component_func = components.declare_component(
     "atc_tracker",
-    path=os.path.join(os.path.dirname(__file__), "frontend", "build")
+    path=os.path.join(os.path.dirname(__file__), "frontend/build")
 )
 
-def atc_tracker(update_interval=60, stream_url=None):
+def atc_tracker(update_interval=60, stream_url=""):
     return _component_func(update_interval=update_interval, stream_url=stream_url, default=0)
--- streamlit_atc_tracker/frontend/webpack.config.js
+++ streamlit_atc_tracker/frontend/webpack.config.js
@@ -1,24 +1,18 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "build"),
     filename: "index.js",
-    libraryTarget: "umd"
+    path: path.resolve(__dirname, "build"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js"]
-  }
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "public/index.html", to: "index.html" }
+      ]
+    }),
+  ],
+  mode: "production"
 };
