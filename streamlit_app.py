b0b3570f6b9cb7941eb100088a07fcd33cb01eb0
Maya Sheth <shethm@DNa207969.SUNet>
2025-06-25 16:57:00 -0700

    time tracking

--- streamlit_app.py
+++ streamlit_app.py
@@ -147,9 +147,10 @@ else:
     # Embed Spotify iframe and fallback button in columns
     col_spotify, col_button = st.columns([7, 2])
     with col_spotify:
-        components.html(f"""
-        <iframe src="{SPOTIFY_PLAYLISTS[playlist]}" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        """, height=100)
+        components.iframe(SPOTIFY_PLAYLISTS[playlist], height=80)
+        # components.html(f"""
+        # <iframe src="{SPOTIFY_PLAYLISTS[playlist]}" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        # """, height=100)
     with col_button:
         playlist_url = SPOTIFY_PLAYLISTS[playlist].split("?")[0]
         st.markdown("**on mobile?**")
