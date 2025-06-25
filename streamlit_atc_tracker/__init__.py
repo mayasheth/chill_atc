018a9b2af86ca14109a3699536a6adda4d09f937
Maya Sheth <shethm@DNa207969.SUNet>
2025-06-24 19:36:00 -0700

    update custom component

--- streamlit_atc_tracker/__init__.py
+++ streamlit_atc_tracker/__init__.py
@@ -1,10 +1,14 @@
-import os
 import streamlit.components.v1 as components
+import os
 
 _component_func = components.declare_component(
     "atc_tracker",
-    path=os.path.join(os.path.dirname(__file__), "frontend/build")
+    path=os.path.join(os.path.dirname(__file__), "build")  # <-- must exist!
 )
 
-def atc_tracker(update_interval=60, stream_url=""):
-    return _component_func(update_interval=update_interval, stream_url=stream_url, default=0)
+def atc_tracker(update_interval=60, stream_url=None):
+    return _component_func(
+        update_interval=update_interval,
+        stream_url=stream_url,
+        default=0,
+    )
